1798928ad93c3cb94a9ae023b49d77ab501d3856
DorZevron <dorzevron90@gmail.com>
2026-03-02 23:26:53 +0200

    feat: add reactive form with email validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ApiService } from './core/services/api.service';
+import { ApiResponse } from './core/models/api-response.interface';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tlv-municipality-task';
+
+  private apiService = inject(ApiService);
+  emailControl = new FormControl('', [Validators.required, Validators.email]);
+  lastResponseTime: string | null = null;
+  errorMessage: string | null = null;
+  isLoading = false;
+
+
+  onSubmit() {
+    this.errorMessage = null;
+
+    if (this.emailControl.invalid) {
+      this.errorMessage = 'אנא הזן כתובת אימייל תקינה.';
+      return;
+    }
+
+    this.isLoading = true;
+    this.apiService.sendEmail({ email: this.emailControl.value! }).subscribe({
+      next: (response: ApiResponse) => {
+        this.lastResponseTime = response.receivedAt;
+        this.isLoading = false;
+      },
+      error: (err: HttpErrorResponse) => {
+        this.isLoading = false;
+        
+        if (err.status === 429 && err.error) {
+          this.lastResponseTime = err.error.receivedAt;
+          this.errorMessage = 'שגיאה (429): תקרת בקשות הושגה. אנא נסה שוב מאוחר יותר.';
+        } else {
+          this.errorMessage = `אירעה שגיאה כללית מול השרת (${err.status}). אנא נסה שוב מאוחר יותר.`;
+        }
+      }
+    });
+  }
 }
